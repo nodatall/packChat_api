c3d428e260c4611f0339171621f6a461af617a44
jason00111 <emberleyjason@gmail.com>
2017-03-12 11:58:42 -0700

    some sockets set up

--- db.js
+++ db.js
@@ -8,12 +8,12 @@ const db = pgp({
 
 const addParent = function (data) {
   console.log('adding parent', data)
-  db.query('INSERT INTO parent (email, first_name, last_name, password, picture_url) VALUES ($1, $2, $3, $4, $5)', [data.email, data.first_name, data.last_name, data.password, data.picture_url])
+  return db.query('INSERT INTO parent (email, first_name, last_name, password, picture_url) VALUES ($1, $2, $3, $4, $5)', [data.email, data.first_name, data.last_name, data.password, data.picture_url])
   .catch(error => console.log('error:', error))
 }
 
 const createPack = function (data) {
-  db.query('INSERT INTO pack (name, description, creator_id) VALUES ($1, $2, $3)', [data.name, data.description, data.creator_id])
+  return db.query('INSERT INTO pack (name, description, creator_id) VALUES ($1, $2, $3)', [data.name, data.description, data.creator_id])
   .catch(error => console.log('error:', error))
 }
 
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const io = require('socket.io')(server)
 const db = require('./db')
 
 
+const connectedParents = []
+  // [{ id: 1, socket: sldkjflk }, { id: 2, socket: lkjlkj }]
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(request, response) {
@@ -20,6 +23,80 @@ app.post('/addParent', function(request, response) {
 app.post('/addPack', function(request, response) {
   db.addPack(request.body)
 })
+
+io.on('connection', function (clientSocket) {
+  console.log('A client connected')
+
+  clientSocket.on('login', function (data) {
+    connectedParents.push({
+      parentId: data.parentId,
+      socket: clientSocket
+    })
+    socket.parentId = data.parentId
+  })
+
+  clientSocket.on('crisis', function (data) {
+    db.getParentsByPack(data.packId)
+    .then(parentIds => {
+      parentIds.forEach(parentId => {
+        getSocketByParentId(parentId).emit('crisis', data.message)
+      })
+    })
+  })
+
+  clientSocket.on('acceptChallenge', function (data) {
+    db.addSaviorToCrisis(data.crisisId, data.parentId)
+
+    db.getParentByCrisis(data.crisisId)
+    .then(parentWithCrisisId => {
+      getSocketByParentId(parentWithCrisisId)
+      .emit('saviorFound', {saviorId: data.parentId})
+    })
+
+    sendAcceptMessageToPack({
+      saviorId: data.parentId,
+      crisisId: data.crisisId
+    })
+  })
+
+  clientSocket.on('refuseChallenge', function (data) {
+    sendRefuseMessageToPack({
+      refuserId: data.parentId,
+      crisisId: data.crisisId
+    })
+  })
+
+})
+
+function getSocketByParentId (parentId) {
+  return connectedParents.find(parent => parent.id === parentId).socket
+}
+
+function sendAcceptMessageToPack(acceptMessageData) {
+  db.getParentsByPack(acceptMessageData.packId)
+  .then(parentIds => {
+    parentIds.forEach(parentId => {
+      sendAcceptMessageToParent(acceptMessageData, parentId)
+    })
+  })
+}
+
+function sendAcceptMessageToParent (acceptMessageData, parentId) {
+  getSocketByParentId(parentId).emit('saviorFound', acceptMessageData)
+}
+
+function sendRefuseMessageToPack(refuseMessageData) {
+  db.getParentsByPack(refuseMessageData.packId)
+  .then(parentIds => {
+    parentIds.forEach(parentId => {
+      sendRefuseMessageToParent(refuseMessageData, parentId)
+    })
+  })
+}
+
+function sendRefuseMessageToParent (refuseMessageData, parentId) {
+  getSocketByParentId(parentId).emit('refuse', refuseMessageData)
+}
 
 server.listen(3002, function () {
   console.log('-:: Listening on port 3002 ::-')
