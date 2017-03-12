eca53c15468cc3a73b8121ec04c3b04704687b4a
David Rothschild <david@rothschildsolutions.com>
2017-03-12 14:27:18 -0700

    add seed file

---
+++ seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO 
+  parents
+VALUES
+ (default, "betty@gmail.com", "Betty", "Draper", "password"),
+ (default, "john@gmail.com", "John", "Smith", "password"),
+ (default, "ringo@gmail.com", "Ringo", "Starr", "password"),
+ (default, "django@gmail.com", "Django", "Reinhardt", "password");
+
+INSERT INTO 
+  packs
+VALUES
+ (default, "Soccer Team 2017", "Go Bulldogs");
+
+
+INSERT INTO 
+  children
+VALUES
+ (default, 1, "Sally");
+
+INSERT INTO 
+  children_packs
+VALUES
+ (default, 1, 1);
+
